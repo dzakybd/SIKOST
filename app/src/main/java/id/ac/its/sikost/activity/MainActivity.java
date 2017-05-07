deb62be8b10126f8f42c5b5f91922cf27a958dba
dzakybd <dzakybd@gmail.com>
2017-05-07 22:12:50 +0700

    -

--- app/src/main/java/id/ac/its/sikost/activity/MainActivity.java
+++ app/src/main/java/id/ac/its/sikost/activity/MainActivity.java
@@ -1,15 +1,19 @@
 package id.ac.its.sikost.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.mikepenz.fontawesome_typeface_library.FontAwesome;
 import com.mikepenz.iconics.IconicsDrawable;
+import com.pixplicity.easyprefs.library.Prefs;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
     ImageView pindahkamar;
     @BindView(R.id.datapembayaran)
     ImageView datapembayaran;
+    @BindView(R.id.keluar)
+    Button keluar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
                 .actionBar());
     }
 
-    @OnClick({R.id.datakamar, R.id.datapenghuni, R.id.pindahkamar, R.id.datapembayaran})
+    @OnClick({R.id.datakamar, R.id.datapenghuni, R.id.pindahkamar, R.id.datapembayaran,R.id.keluar})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.datakamar:
@@ -67,6 +73,29 @@ public class MainActivity extends AppCompatActivity {
             case R.id.datapembayaran:
                 startActivity(new Intent(MainActivity.this, DataPembayaranActivity.class));
                 break;
+            case R.id.keluar:
+                out();
+                break;
         }
+    }
+
+    private void out() {
+        AlertDialog.Builder pilihan = new AlertDialog.Builder(this);
+        pilihan.setMessage("Anda ingin keluar?");
+        pilihan.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Prefs.clear();
+                startActivity(new Intent(MainActivity.this, Login.class));
+                finish();
+            }
+        });
+        pilihan.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+            }
+        });
+        AlertDialog alert = pilihan.create();
+        alert.show();
     }
 }
